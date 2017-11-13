2aa7fe4ae48f56134b14f26939f5a851c4e3afa8
Haraldur Gunnarsson <Haraldur Gunnarsson>
2017-11-13 21:47:00 +0000

    added socket.io

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,28 @@
 import * as http from 'http';
 import App from './App';
+import * as socketIo from "socket.io";
+
 const port =  4000;
+
 App.set('port', port);
 //create a server and pass our Express app to it.
 const server = http.createServer(App);
 server.listen(port);
 server.on('listening', onListening);
+
+let io = socketIo(server);
+
+io.on('connect', (socket: any) => {
+            console.log('Connected client on port %s.', this.port);
+            socket.on('message', (m: string) => {
+                console.log('[server](message): %s', JSON.stringify(m));
+                this.io.emit('message', m);
+            });
+
+            socket.on('disconnect', () => {
+                console.log('Client disconnected');
+            });
+        });
 
 //function to note that Express is listening
 function onListening(): void {
